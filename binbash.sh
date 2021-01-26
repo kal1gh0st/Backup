38866d0870fe8053eb4cc93dd23ceee3c42afc27
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-01-26 13:44:56 -0500

    Update binbash.sh

--- binbash.sh
+++ binbash.sh
@@ -7,7 +7,8 @@
 #typically the first line of the bash scripts
 #BACKUPTIME=`date +%b-%d-%y` #get the current date
 BACKUPTIME='date +%b-%d-%y'
-DESTINATION='/home/kalighost/backup/backup-$BACKUPTIME.tar.gz' #create a backup file using the current date in it's name
-SOURCEFOLDER='/home/kalighost/' #the folder that contains the files that we want to backup
+DESTINATION='/root/backup/backup-$BACKUPTIME.tar.gz' #create a backup file using the current date in it's name
+SOURCEFOLDER='/opt/Crack' #the folder that contains the files that we want to backup
 tar -cpzf $DESTINATION $SOURCEFOLDER #create the backup
-ls -lah /home/kalighost/backup/backup-$BACKUPTIME.tar.gz
+cat /root/Desktop/suc
+ls -lah /root/backup/
